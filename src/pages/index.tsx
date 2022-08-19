8d324d9315867331cf93f1e872d93c0c2e9dd732
diegosales30 <diegofelipesales23@gmail.com>
2022-08-19 14:02:00 -0400

    finalizando home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,45 @@ import style from "../styles/styles.module.scss";
 
 export default function Home() {
   return (
-    <>
+    <div
+      style={{
+        backgroundColor: "#3ad586",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
       <Head>
-        <title>Task io</title>
+        <title>Home</title>
       </Head>
 
-      <div>
-        <h1 className={style.title}>
-          ola
-          <span>mundo</span>
-        </h1>
-      </div>
-    </>
+      <main className={style.contentContainer}>
+        <img
+          className={style.imgHome}
+          src="/images/gifLanding.gif"
+          alt="home"
+        />
+        <section className={style.callToAction}>
+          <h1>Organize seu dia com seu gerenciador de tarefas</h1>
+          <p>
+            <span>Gratis</span> e online
+          </p>
+        </section>
+
+        <div className={style.doadores}>
+          <img
+            src="https://avatars.githubusercontent.com/u/95250284?v=4"
+            alt="user1"
+          />
+          <img
+            src="https://avatars.githubusercontent.com/u/95250284?v=4"
+            alt="user1"
+          />
+          <img
+            src="https://avatars.githubusercontent.com/u/95250284?v=4"
+            alt="user1"
+          />
+        </div>
+      </main>
+    </div>
   );
 }
---
+++ src/pages/tasks.tsx
@@ -0,0 +1,14 @@
+import Head from "../../node_modules/next/head";
+
+export default function MyTasks() {
+  return (
+    <>
+      <Head>
+        <title>Task</title>
+      </Head>
+      <div>
+        <h1>My Tasks</h1>
+      </div>
+    </>
+  );
+}
